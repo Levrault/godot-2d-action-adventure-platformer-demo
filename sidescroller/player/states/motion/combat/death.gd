ca23e8dde5a90acb72281009676f0cb5ae7c20a7
unknown <lucf.langis@gmail.com>
2019-04-22 14:58:08 +0200

    feat(player): add explosion on death

--- sidescroller/player/states/motion/combat/death.gd
+++ sidescroller/player/states/motion/combat/death.gd
@@ -11,4 +11,7 @@ func enter(host: Player) -> void:
 
 func _on_animation_finished(anim_name: String, host: Player) -> void:
 	assert anim_name == 'Death'
+	var explosion := EXPLOSION_SCENE.instance()
+	explosion.set_position(host.get_global_position())
+	get_tree().get_root().add_child(explosion)
 	host.queue_free()
--- sidescroller/sfx/Explosion.gd
+++ sidescroller/sfx/Explosion.gd
@@ -4,7 +4,9 @@ extends Node2D
 func _ready(): 
 	#warning-ignore:return_value_discarded
 	$AnimationPlayer.connect('animation_finished', self, '_on_Animation_finished')
+	$AnimationPlayer.play('Explode')
 
 
-func _on_Animation_finished():
+func _on_Animation_finished(anim_name: String):
+	assert anim_name == 'Explode'
 	queue_free()
