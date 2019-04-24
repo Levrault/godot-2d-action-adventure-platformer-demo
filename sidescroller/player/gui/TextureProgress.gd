b97286ac98402ebd44d97fe47a8534bc719417f4
Luc-Frédéric Langis <lucf.langis@gmail.com>
2019-04-24 14:26:25 +0200

    feat(gui): improve health bar animation

--- sidescroller/player/gui/TextureProgress.gd
+++ sidescroller/player/gui/TextureProgress.gd
@@ -1,6 +1,24 @@
+tool
 extends TextureProgress
+
+export(Color) var COLOR setget set_color_normal
+
+func _ready():
+	$TweenColor.connect('tween_completed', self, '_on_tween_color_completed')
 
 
 func update_value(new_value):
-	$Tween.interpolate_property(self, 'value', value, new_value, 0.3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
-	$Tween.start()
+	$TweenColor.interpolate_property(self, 'modulate', modulate, Color(255, 255, 255), 0.4, Tween.TRANS_LINEAR, Tween.EASE_OUT_IN)
+	$TweenColor.start()
+	
+	$TweenProgress.interpolate_property(self, 'value', value, new_value, 0.3, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$TweenProgress.start()
+
+
+func _on_tween_color_completed(object: Object, key: NodePath):
+	set_color_normal(COLOR)
+	
+	
+func set_color_normal(value):
+	COLOR = value
+	modulate = value
