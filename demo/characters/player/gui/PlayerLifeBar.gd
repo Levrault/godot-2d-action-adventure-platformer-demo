c46b0c1e5d3b8cc182723c5e2b0a5261b5e0fec6
Luc-Frédéric Langis <lucf.langis@gmail.com>
2019-05-27 13:19:49 +0200

    feat(player): improve health bar animation

--- demo/characters/player/gui/PlayerLifeBar.gd
+++ demo/characters/player/gui/PlayerLifeBar.gd
@@ -1,47 +1,27 @@
 extends TextureRect
 
-const SHAKE_TRIGGER = 25
-const SHAKE_DISTANCE = 4
-var shake_health_bar := false
-var initial_margin_top := 0.0
+const LOW_HEALTH_TRIGGER = 25
+var is_health_low: bool = false
 
 
 func _ready() -> void:
-	$TweenShakeDown.connect('tween_completed', self, '_on_tween_shake_down_completed')
-	$TweenShakeUp.connect('tween_completed', self, '_on_tween_shake_up_completed')
-	get_parent().get_parent().get_node('Health').connect('health_changed', self, '_on_Health_changed')
-	initial_margin_top = margin_top
+	$AnimationPlayer.connect('animation_finished', self, '_on_Animation_finished')
+	if get_parent().get_parent().has_node('Health'):
+		get_parent().get_parent().get_node('Health').connect('health_changed', self, '_on_Health_changed')
 
 
-func _on_Health_changed(health_value) -> void:
+func _on_Health_changed(health_value: float) -> void:
 	$TextureProgress.update_value(health_value)
+	is_health_low = health_value <= LOW_HEALTH_TRIGGER
+	$AnimationPlayer.play('Shake')
 	
-	shake_health_bar = health_value <= SHAKE_TRIGGER
+
+func _on_Animation_finished(anim_name: String) -> void:
+	match anim_name:
+		'Shake':
+			if is_health_low:
+				$AnimationPlayer.play('Flash')
+		'Flash':
+			if not is_health_low:
+				$AnimationPlayer.stop('Flash')
 	
-	if shake_health_bar:
-		_on_tween_shake_down_completed(self, 'margin_top')
-
-
-# warning-ignore:unused_argument
-# warning-ignore:unused_argument
-func _on_tween_shake_down_completed(object: Object, key: NodePath):
-	if shake_health_bar:
-		$TweenShakeUp.interpolate_property(self, 'margin_top', margin_top, margin_top - SHAKE_DISTANCE, 0.1, Tween.TRANS_LINEAR, Tween.EASE_OUT)
-		$TweenShakeUp.start()
-	else:
-		_reset_margin_top()
-
-
-# warning-ignore:unused_argument
-# warning-ignore:unused_argument
-func _on_tween_shake_up_completed(object: Object, key: NodePath):
-	if shake_health_bar:
-		$TweenShakeDown.interpolate_property(self, 'margin_top', margin_top, margin_top + SHAKE_DISTANCE, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN)
-		$TweenShakeDown.start()
-	else:
-		_reset_margin_top()
-
-
-func _reset_margin_top():
-	$TweenReset.interpolate_property(self, 'margin_top', margin_top, initial_margin_top, 0.1, Tween.TRANS_LINEAR, Tween.EASE_IN)
-	$TweenReset.start()
