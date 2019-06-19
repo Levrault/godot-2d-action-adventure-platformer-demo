1f266f84a9ea5dc6e69033f2f25730414b1107ef
Luc-Frédéric Langis <llangis@splio.com>
2019-06-19 16:50:22 +0200

    feat(house): add next/end alert box

--- demo/environment/props/House.gd
+++ demo/environment/props/House.gd
@@ -4,7 +4,9 @@ class_name Interaction
 signal interaction(type)
 
 const ALERT_SCENE: Resource = preload('res://interfaces/dialogue/AlertBox.tscn')
+const NEXT_ALERT_SCENE: Resource = preload('res://interfaces/dialogue/AlertNextLevelBox.tscn')
 export(String) var scene_path = ''
+export(bool) var is_last_level = false
 
 
 func _ready():
@@ -14,7 +16,7 @@ func _ready():
 func _on_body_enter(body: Object) -> void:
 	if body.get_collision_mask_bit(1):
 		if body.is_grounded:
-			var alert_box = ALERT_SCENE.instance()
+			var alert_box = NEXT_ALERT_SCENE.instance() if not is_last_level else ALERT_SCENE.instance()
 			alert_box.set_scene_path(scene_path)
 			alert_box.position = $AlertBoxPosition.get_global_position()
 			get_tree().get_root().add_child(alert_box)
